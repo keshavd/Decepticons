4eb1fb2480b39b86d8046e3bd97ca73313275999
Keshav Dial <y.inuyasha@gmail.com>
2021-10-21 21:48:47 -0400

    removing loss calculation from the head.

--- src/decepticons/heads/TokenClassificationCrfHead.py
+++ src/decepticons/heads/TokenClassificationCrfHead.py
@@ -11,15 +11,24 @@ class TokenClassificationCrfHead(nn.Module):
         self.classifier = nn.Linear(config.hidden_size, config.num_labels)
         self.crf = CRF(config.num_labels, batch_first=True)
 
-    def forward(self, sequence_output, tags, mask=None, reduction="sum"):
+    def forward(self, sequence_output):
+        """ Returns logits from classification layer"""
+        x = self.dropout(sequence_output)
+        x = self.classifier(x)
+        return x
+
+    def predict(self, sequence_output, attention_mask=None):
+        """Returns the calculated labels via viterbi algo
+
+        One-hot encoded to fake logits
+        """
+        x = self.classifier.eval()(sequence_output)
+        x = self.crf.decode(emissions=x, mask=attention_mask)
+        return F.one_hot(x, num_classes=self.crf.num_tags)
+
+    def get_loss(self, sequence_output, tags, mask=None, reduction="sum"):
         """ Returns negative log-likelihood"""
         x = self.dropout(sequence_output)
         x = self.classifier(x)
         x = self.crf(emissions=x, mask=mask, tags=tags, reduction=reduction)
         return torch.neg(x)
-
-    def predict(self, sequence_output, mask=None):
-        """ Returns the calculated labels via viterbi algo"""
-        x = self.classifier.eval()(sequence_output)
-        x = self.crf.decode(emissions=x, mask=mask)
-        return F.one_hot(x, num_classes=self.crf.num_tags)
